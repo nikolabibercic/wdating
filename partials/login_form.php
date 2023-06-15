111abf86e7a668a046e8e69bfca4347b6d8b7bf5
Danijela Glušac <nikolabibercic@gmail.com>
2023-06-16 00:26:30 +0200

    stilizovanje log in forme

--- partials/login_form.php
+++ partials/login_form.php
@@ -1,11 +1,13 @@
-<form class="loginForm container" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+<form class="form container" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
     <?php if(isset($emailError)): ?>
-        <p>Email is required!</p>
+        <p style="color:var(--danger); font-weight:600;">Email is required!</p>
     <?php endif; ?>
     <input type="text" name="email" placeholder="Email">
     <?php if(isset($passwordError)): ?>
-        <p>Password is required!</p>
+        <p style="color:var(--danger); font-weight:600;">Password is required!</p>
     <?php endif; ?>
     <input type="password" name="password" placeholder="Password">
     <button type="submit">Log in</button>
+    <p>Don't have account? <a href="register.php">Create account here.</a></p>
 </form>
+
